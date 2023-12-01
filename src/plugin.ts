6c72bd3bf9d0c3554ad91add4ed78a6a75a7746b
mochenya <2827075398@qq.com>
2023-12-01 20:03:33 +0800

    add tauri package

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,22 @@
-import { PiniaPlugin, PiniaPluginContext } from "pinia";
+import {PiniaPlugin, PiniaPluginContext, StateTree} from "pinia";
+import {fs} from "@tauri-apps/api";
 
-export function createPersistedState(
-
-): PiniaPlugin {
+export function createPersistedState(): PiniaPlugin {
   return (context: PiniaPluginContext) => {
     console.log('context', context);
+    context.store.$subscribe((mutation, state: StateTree) => {
+      console.log('mutation', mutation);
+      console.log('state', state);
+      fs.writeFile({
+        path: 'test.txt',
+        contents: 'Hello World!'
+      })
+          .then(() => {
+            console.log('The file has been written!');
+          })
+          .catch(error => {
+            console.error("Error: ", error);
+          })
+    });
   }
 }
